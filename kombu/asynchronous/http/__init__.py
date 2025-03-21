140ee457b9b0410cea897555089975aa7cb43d23
Paul Rysiavets <paul@covadem.com>
2025-03-21 15:55:51 +0100

    test(urllib3): switch to pycurl

--- kombu/asynchronous/http/__init__.py
+++ kombu/asynchronous/http/__init__.py
@@ -9,6 +9,9 @@ __all__ = ('Client', 'Headers', 'Response', 'Request', 'get_client')
 
 def Client(hub: Hub | None = None, **kwargs: int) -> BaseClient:
     """Create new HTTP client."""
+    from .curl import CurlClient
+    return CurlClient(hub, **kwargs)
+
     from .urllib3_client import Urllib3Client
     return Urllib3Client(hub, **kwargs)
 
